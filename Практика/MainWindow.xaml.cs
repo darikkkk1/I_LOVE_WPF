e39507784c1cd664e6a35e231e7f9c79427b066b
darikkkk1 <73314815+darikkkk1@users.noreply.github.com>
2021-10-30 18:39:11 +0300

    Add files via upload

---
+++ Практика/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практика
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelPage());
+            Manager.MainContent = MainContent;
+            MainContent.Navigate(new Inform());
+
+        }
+
+        private void ButtonForMenu(object sender, RoutedEventArgs e)
+        {
+            MainContent.Navigate(new Inform());
+        }
+    }
+}
---
+++ Практика/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Практика
+{
+    class Manager
+    {
+        public static Frame MainContent { get; set; }
+    }
+}
